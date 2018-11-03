6928bd3a63472f01ca3a5f79482faaaca2824dbd
Yuhao Fang <fangyuhao@gmail.com>
2018-11-03 16:39:38 +0800

    adds client id

--- pkg/types/rpc.go
+++ pkg/types/rpc.go
@@ -3,11 +3,13 @@ package types
 import "encoding/json"
 
 type Request struct {
-	Method string          `json:"method"`
-	Args   json.RawMessage `json:"args,omitempty"` // TODO consider other proto
+	Method   string          `json:"method"`
+	ClientId int64           `json:"clientId,omitempty"`
+	Args     json.RawMessage `json:"args,omitempty"` // TODO consider other proto
 }
 
 type Response struct {
-	Status int    `json:"status"` // 0 ok, 1 ping, > 1 errors
-	Data   []byte `json:"data,omitempty"`
+	Status   int    `json:"status"` // 0 ok, 1 ping, > 1 errors
+	ClientId int64  `json:"clientId,omitempty"`
+	Data     []byte `json:"data,omitempty"`
 }
